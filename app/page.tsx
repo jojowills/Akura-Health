0da1ba4237a50b4f0d28c4cdae7df3b63175597c
Josette Wilson <josettewilson@Josettes-MBP.communityfibre.co.uk>
2026-05-17 22:10:36 +0100

    connect waitlist form

--- app/page.tsx
+++ app/page.tsx
@@ -36,7 +36,7 @@ export default function Home() {
 
         {/* Waitlist Form */}
         <div id="waitlist" className="w-full max-w-md">
-          <form className="flex flex-col sm:flex-row gap-3">
+          <<form action="https://formspree.io/f/mykvyzvw" method="POST" className="flex flex-col sm:flex-row gap-3"></>>
             <input
               type="email"
               placeholder="Enter your email"
